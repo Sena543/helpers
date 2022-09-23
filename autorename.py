95be2f2e2e5f656a4969255a43b43ea7b6ba1d3b
Senanu <42738493+Sena543@users.noreply.github.com>
2022-09-23 13:05:08 +0000

    Auto rename python file
    for renaming srt files to the corresponding video files
    some changes needed though

---
+++ autorename.py
@@ -0,0 +1,15 @@
+import os
+
+file_path = "Ozark.S01.COMPLETE.720p.NF.WEBRip.x264-GalaxyTV[TGx]/"
+# print(os.getcwd())
+fileNames = [names for _, __, names in os.walk(file_path)][0]
+srtFiles, vidFiles = fileNames[: len(
+    fileNames)//2], fileNames[len(fileNames)//2:]
+print(srtFiles)
+print("============")
+print(vidFiles)
+for _, __, files in os.walk(file_path):
+    for index, name in enumerate(files, start=0):
+        if name.endswith('.srt'):
+            os.rename(os.path.join(os.getcwd(), file_path, name),
+                      os.path.join(os.getcwd(), file_path, vidFiles[index].replace('.mkv', '.srt')))
