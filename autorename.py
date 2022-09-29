b167bb66e64c51e6483cc7ba9ff9ffa3a752f78d
Senanu <42738493+Sena543@users.noreply.github.com>
2022-09-29 15:09:13 +0000

    ...

--- autorename.py
+++ autorename.py
@@ -1,17 +1,39 @@
 import os
+# import itertools as iter
 
 print('Please make sure script file is folder level above video files and all srt file are in the same folder as the videos.')
 file_path = input('Enter folder name:')
-# file_path = "Ozark.S01.COMPLETE.720p.NF.WEBRip.x264-GalaxyTV[TGx]/"
-# print(os.getcwd())
-fileNames = [names for _, __, names in os.walk(file_path)][0]
-srtFiles, vidFiles = fileNames[: len(
-    fileNames)//2], fileNames[len(fileNames)//2:]
-print(srtFiles)
-print("============")
-print(vidFiles)
-for _, __, files in os.walk(file_path):
-    for index, name in enumerate(files, start=0):
-        if name.endswith('.srt'):
-            os.rename(os.path.join(os.getcwd(), file_path, name),
-                      os.path.join(os.getcwd(), file_path, vidFiles[index].replace('.mkv', '.srt')))
+fileNames = os.listdir(file_path)
+srtFiles = []
+vidFiles = []
+
+
+def splitFilesInFolderToArray(srt_files=None, vid_files=None):
+    # srt_files, vid_files = srtFiles, vidFiles
+    if srt_files is None or vid_files is None:
+        return
+
+    for el in fileNames:
+        # print(el)
+        if el.endswith('.srt'):
+            srt_files.append(el)
+        else:
+            vid_files.append(el)
+    return srt_files, vid_files
+
+
+# print(splitFilesInFolderToArray(srtFiles, vidFiles))
+
+
+def renameFiles():
+    (srt_Files, vid_Files) = splitFilesInFolderToArray(srtFiles, vidFiles)
+    print(srt_Files)
+    for srt, vid in zip(srt_Files, vid_Files):
+        os.rename(os.path.join(os.getcwd(), file_path, srt),
+                  os.path.join(os.getcwd(), file_path, vid.replace('.mkv', '.srt')))
+    return
+
+
+if __name__ == "__main__":
+    renameFiles()
+    # pass
